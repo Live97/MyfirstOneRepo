9d185fbbf8b517cc89ef721f705639da05962a55
Live97 <35063626+Live97@users.noreply.github.com>
2018-10-06 01:16:08 +0800

    Update nice_day.cpp

--- nice_day.cpp
+++ nice_day.cpp
@@ -6,5 +6,6 @@ int main()
 	cout<<"An nice day!"<<endl;
 	cout<<"An another day,but i don't know how do you passed<<endl";
 	cout<<"Today is also filled with hope"<<endl;
+	cout<<"oh!no in test"<<endl;
 	return 0;
 }
