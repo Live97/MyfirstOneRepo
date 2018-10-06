cc5484d1a9704b21c33d77f7d4f1a10a5c0928c5
AmgLi97 <Live97@AMG.com>
2018-10-06 16:48:17 +0800

    修改内容

--- string_test.cpp
+++ string_test.cpp
@@ -5,4 +5,19 @@
 #include <windows.h>
 using namespace std;
 
+int main()
+{
+    //constructors构造函数
+    string str1(5,'c');
+    string str2("Now is the time...");
+    string str3(str2,11,4);
+    cout<<str1<<endl;
+    cout<<str2<<endl;
+    cout<<str3<<endl;
 
+    str1.push_back('e');
+    str1.insert(2,"vv");
+    str1.append("123");
+    str1.resize(20,'*');
+    cout<<str1<<endl;
+}
