7631fe7c427cbbc91d646e40075d477a3d63a144
AmgLi97 <Live97@AMG.com>
2018-10-07 23:31:06 +0800

    2018.10.7 23:31

---
+++ constclass_test.cpp
@@ -0,0 +1,26 @@
+﻿//#include <iostream>
+//#include <stdlib.h>
+//using namespace std;
+
+//class Sample
+//{
+//public:
+//    int value;
+//    Sample(){}
+//    void ssp( Sample & obj2 ) const;
+//    void ssd( const Sample & Obj1);
+//};
+//void Sample::ssp( Sample & obj2 ) const
+//{
+//    cout<<obj2.value<<endl;
+//}
+//void Sample::ssd(const Sample &Obj1)
+//{
+//    Obj1.value = 5;//Wrong
+//}
+//int main()
+//{
+//    Sample s1;
+//    s1.value = 10;
+//    s1.ssp(s1);
+//}
---
+++ friends_test.cpp
@@ -0,0 +1,23 @@
+﻿//#include <iostream>
+
+//using namespace std;
+//class CCar
+//{
+//private:
+//    int price;
+//    friend class CDriver;
+//};
+
+//class CDriver
+//{
+//public:
+//    CCar MyCar;
+//    void ModifyCar(  )
+//    {
+//        MyCar.price += 1000;
+//    }
+//};
+//int main()
+//{
+
+//}
