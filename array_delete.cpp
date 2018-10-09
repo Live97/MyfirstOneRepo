e92412ce0dcaf8d63412f1ac8dc9177a2aa8d961
AmgLi97 <Live97@AMG.com>
2018-10-09 21:40:40 +0800

    2018.10.9 21:40

--- array_delete.cpp
+++ array_delete.cpp
@@ -1,4 +1,5 @@
 ﻿//#include <iostream>
+//#include <vector>
 //using namespace std;
 
 //int Delete( int arr[],int n,int & m )
@@ -15,24 +16,40 @@
 //}
 //void insert( int arr[],int n,int x)
 //{
+//    int ins;
+//    cout<<"Where the position you wanto insert: ";
+//    cin>>ins;
 //    for( int i = 0; i < n; i++ ){
-//        ;
+
+//        if( ins == n-1 )
+//        {
+//            cout<<"I'm sorry this Array is filled"<<endl;
+//            return ;
+//        }else if( i == ins ){
+//            for( int j = n-1; j >= ins; j-- ){
+//                arr[j+1] = arr[j];
+//                arr[i] = x;
+//                return ;
+//            }
+//        }
 //    }
 //}
-//void Print( int arr[],int & m )
+//void Print( int arr[],int n )
 //{
-//    for( int i = 0; i <= m-1; i++ )
-//        cout<<arr[i]<<" ";
+//    for( int i = 0; i <= n; i++ )
+//        if( arr[i] )
+//            cout<<arr[i]<<" ";
 //}
 
 //int main()
 //{
 
-//    int arr[] = {1,2,3,4,5,6,6,3,12,123,41,222,3,2,3,1,3,5,2,3};
-//    int n = sizeof(arr) / sizeof(int)+1;
+//    int arr[11] = {1,2,3,4,5,6,7,8,9,10};
+//    int n = sizeof(arr) / sizeof(int);
 //    int &m = n;
-////    Delete(arr,n,m);
+//    Delete(arr,n,m);
 //    insert(arr,n,30);
-//    Print(arr,m);
+//    Print(arr,n);
+
 
 //}
---
+++ link_list_delete.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#define ElementType  int
+#define MAXN 1000
+using namespace std;
+
+typedef struct Link_list {
+    ElementType value;
+    struct Link_list *Next;
+}link;
+
+typedef struct _link {
+    link *head;
+}Node;
+
+void Assign( Node *Ptrl )
+{
+    link *p,*q;
+    int member;
+    cin>>member;
+    int i,data;
+    for( i = 1; i <= member; i++ ){
+        cin>>data;
+        p = ( link* )malloc( sizeof(link));
+        p->value = data;
+        p->Next = NULL;
+
+        if( Ptrl->head == NULL )
+            Ptrl->head = p;
+        else
+            q->Next = p;
+
+        q = p;
+    }
+}
+void FoundNumber( Node *Ptrl,int k)
+{
+    link *p = Ptrl->head;
+    int i = 1;
+    while( p != NULL && i != k ){
+        p = p->Next;
+        i++;
+    }
+    if( i == k)
+        cout<<p->value<<endl;
+    else
+        cout<<"Not found"<<endl;
+}
+void Print( Node *Ptrl )
+{
+    link *p;
+    p = Ptrl->head;
+    while( p ){
+        cout<<p->value<<" ";
+        p = p->Next;
+    }
+    cout<<endl;
+}
+void Insert( Node *Ptrl )
+{
+    link *p,*q;
+
+}
+int main()
+{
+    Node form;
+    form.head = NULL;
+    Assign( &form );
+    Print( &form );
+    FoundNumber( &form,3);
+}
