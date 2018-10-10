6b972f129177c7a2b7bf0913f6fc5f97d6cd0619
AmgLi97 <Live97@AMG.com>
2018-10-11 00:49:48 +0800

    2018.10.11 0:49 it's hard..

--- link_list_delete.cpp
+++ link_list_delete.cpp
@@ -1,111 +1,111 @@
-﻿#include <iostream>
-#include <string>
-#include <stdlib.h>
-#include <vector>
-#define ElementType  int
-#define MAXN 1000
-using namespace std;
+﻿//#include <iostream>
+//#include <string>
+//#include <stdlib.h>
+//#include <vector>
+//#define ElementType  int
+//#define MAXN 1000
+//using namespace std;
 
-typedef struct Link_list {
-    ElementType value;
-    struct Link_list *Next;
-}link;
+//typedef struct Link_list {
+//    ElementType value;
+//    struct Link_list *Next;
+//}link;
 
-typedef struct _link {
-    link *head;
-}Node;
+//typedef struct _link {
+//    link *head;
+//}Node;
 
-void Assign( Node *Ptrl )
-{
-    link *p,*q;
-    int member;
-    cin>>member;
-    int i,data;
-    for( i = 1; i <= member; i++ ){
-        cin>>data;
-        p = ( link* )malloc( sizeof(link));
-        p->value = data;
-        p->Next = NULL;
+//void Assign( Node *Ptrl )
+//{
+//    link *p,*q;
+//    int member;
+//    cin>>member;
+//    int i,data;
+//    for( i = 1; i <= member; i++ ){
+//        cin>>data;
+//        p = ( link* )malloc( sizeof(link));
+//        p->value = data;
+//        p->Next = NULL;
 
-        if( Ptrl->head == NULL )
-            Ptrl->head = p;
-        else
-            q->Next = p;
+//        if( Ptrl->head == NULL )
+//            Ptrl->head = p;
+//        else
+//            q->Next = p;
 
-        q = p;
-    }
-}
-void FoundNumber( Node *Ptrl,int k)
-{
-    link *p = Ptrl->head;
-    int i = 1;
-    while( p != NULL && i != k ){
-        p = p->Next;
-        i++;
-    }
-    if( i == k)
-        cout<<p->value<<endl;
-    else
-        cout<<"Not found"<<endl;
-}
-void Print( Node *Ptrl )
-{
-    link *p;
-    p = Ptrl->head;
-    while( p ){
-        cout<<p->value<<" ";
-        p = p->Next;
-    }
-    cout<<endl;
-}
-void Insert( Node *Ptrl ,int k)
-{
-    link *p,*q;
-    int InsetNum,i = 1;
-    cin>>InsetNum;
-    for(  p = Ptrl->head; p != NULL; p = p->Next,i++ ){
+//        q = p;
+//    }
+//}
+//void FoundNumber( Node *Ptrl,int k)
+//{
+//    link *p = Ptrl->head;
+//    int i = 1;
+//    while( p != NULL && i != k ){
+//        p = p->Next;
+//        i++;
+//    }
+//    if( i == k)
+//        cout<<p->value<<endl;
+//    else
+//        cout<<"Not found"<<endl;
+//}
+//void Print( Node *Ptrl )
+//{
+//    link *p;
+//    p = Ptrl->head;
+//    while( p ){
+//        cout<<p->value<<" ";
+//        p = p->Next;
+//    }
+//    cout<<endl;
+//}
+//void Insert( Node *Ptrl ,int k)
+//{
+//    link *p,*q;
+//    int InsetNum,i = 1;
+//    cin>>InsetNum;
+//    for(  p = Ptrl->head; p != NULL; p = p->Next,i++ ){
 
-        if( i == k ){
-            p = ( link* )malloc( sizeof(link) );
-            p->value = InsetNum;
-            p->Next = q->Next;
-            q->Next = p;
-        }
-        q = p;
-    }
-}
-void Delete( Node *Ptrl )
-{
-    int DeleNum;
-    cin>>DeleNum;
-    link *p,*q;
-    for( p = Ptrl->head; p != NULL; p = p->Next ){
+//        if( i == k ){
+//            p = ( link* )malloc( sizeof(link) );
+//            p->value = InsetNum;
+//            p->Next = q->Next;
+//            q->Next = p;
+//        }
+//        q = p;
+//    }
+//}
+//void Delete( Node *Ptrl )
+//{
+//    int DeleNum;
+//    cin>>DeleNum;
+//    link *p,*q;
+//    for( p = Ptrl->head; p != NULL; p = p->Next ){
 
-        if( p->value == DeleNum ){
-            //Border Question: if i wan'to Delete the first one Element
-            if( ){
+//        if( p->value == DeleNum ){
+//            //Border Question: if i wan'to Delete the first one Element
+//            if( ){
 
 
-            }else{
-                q->Next = p->Next;
-                free(p);
-                return ;
-            }
-        }
-        q = p;
-    }
-    cout<<"No found this number"<<endl;
+//            }else{
+//                q->Next = p->Next;
+//                free(p);
+//                return ;
+//            }
+//        }
+//        q = p;
+//    }
+//    cout<<"No found this number"<<endl;
 
-}
-int main()
-{
-    Node form;
-    form.head = NULL;
-    Assign( &form );
-    Print( &form );
-    FoundNumber( &form,3);
-    Insert( &form ,3);
-    Print( &form );
-    Delete( &form );
-    Print( &form );
-}
+//}
+//int main()
+//{
+//    Node form;
+//    form.head = NULL;
+//    Assign( &form );
+//    Print( &form );
+//    FoundNumber( &form,3);
+//    Insert( &form ,3);
+//    Print( &form );
+//    Delete( &form );
+//    Print( &form );
+//}
---
+++ max_sequence.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+  int arr[10001];
+  int n;
+  int MaxSum = 0,MaxSumNe = 0;
+  int count = 0;
+  int MinNum = 0,MaxNum = 0;
+  int flag1 = 1,flag3 = 0,i;
+  cin>>n;
+  for( int i = 0; i < n; i++ ){
+    cin>>arr[i];
+  }
+  for( int i = 0; i < n; i++ )
+  {
+      if( arr[i] > 0 )
+          flag3++;
+  }
+  for(  i = 0; i < n; i++ ){
+    count += arr[i];
+    if( count > MaxSum ){
+        MaxSum = count;
+        MaxNum = arr[i]; // already find the MaxNum
+    }
+    else if( count < 0 ){
+        count = 0;
+        if( arr[i+1] > 0 && flag1 == 1){
+             MinNum = arr[i+1];
+             flag1 = 0;
+        }
+    }
+}
+  if( flag3 == 0 )
+      cout<<MaxSumNe<<" "<<arr[0]<<" "<<arr[n-1];
+  else
+      cout<<MaxSum<<" "<<MinNum<<" "<<MaxNum;
+
+
+
+  return 0;
+}
