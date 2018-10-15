47a9f0c42f73a44f9a3b1b42d43af9b69da1861f
AmgLi97 <Live97@AMG.com>
2018-10-16 00:01:29 +0800

    2018.10.16 0:01

--- pointer_test1.cpp
+++ pointer_test1.cpp
@@ -1,40 +1,40 @@
-﻿#include <iostream>
-#include <stdlib.h>
-#include <algorithm>
-#include <string>
-using namespace std;
+﻿//#include <iostream>
+//#include <stdlib.h>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
 
-size_t Strlen( char *string ){
-    int length = 0;
+//size_t Strlen( char *string ){
+//    int length = 0;
 
-    while( *string++ != '\0' )
-        length += 1;
+//    while( *string++ != '\0' )
+//        length += 1;
 
-    return length;
-}
-#define TRUE    1
-#define FALSE   0
+//    return length;
+//}
+//#define TRUE    1
+//#define FALSE   0
 
-int find_char( char **strings,char value ){
+//int find_char( char **strings,char value ){
 
-    char *string;
+//    char *string;
 
-    while( ( string = *strings++ ) != NULL ){
+//    while( ( string = *strings++ ) != NULL ){
 
-        while( *string != '\0' ){
-            if( *string++ == value )
-                return TRUE;
-        }
-    }
-    return FALSE;
-}
-int main()
-{
-    char *str1 = "abced";
-    char **str2 = &str1;
-    cout<<Strlen(str1)<<endl;
-    cout<<find_char( str2,'b');
+//        while( *string != '\0' ){
+//            if( *string++ == value )
+//                return TRUE;
+//        }
+//    }
+//    return FALSE;
+//}
+//int main()
+//{
+//    char *str1 = "abced";
+//    char **str2 = &str1;
+//    cout<<Strlen(str1)<<endl;
+//    cout<<find_char( str2,'b');
 
 
 
-}
+//}
