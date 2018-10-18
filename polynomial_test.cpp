e510db6ace414c362f7a9a41b969c08b1e3d6cf9
AmgLi97 <Live97@AMG.com>
2018-10-18 21:29:58 +0800

    2018.10.18 21:29

---
+++ polynomial_test.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef struct SNode{
+  int XiShu;
+  int ZiShu;
+  struct SNode *next;
+}List;
+void Push( int x,int z,List *Ptrl );
+List *Create(){
+
+    List *NewStack,*tmp,*del;
+    //老师的建议的做法先申请一个空节点,以后需要添加的结点都放在它后面,最后再free掉
+    //我在想有没有其他的方法，想法是有的...但是暂时没能实现...
+    NewStack = (List*)malloc( sizeof(List) );
+    NewStack->next = NULL;
+    int count,zishu,xishu;
+    cin>>count;
+    tmp = NewStack;
+    for( int i = 1; i <= count; i++ ){
+        cin>>xishu>>zishu;
+        Push(xishu,zishu,tmp);
+    }
+    del = NewStack;
+    NewStack = NewStack->next;
+    free(del);
+    return NewStack;
+}
+void Push( int x,int z,List *Ptrl ){
+
+    List *p,*q;
+    p = (List*)malloc( sizeof(List) );
+    p->XiShu = x;
+    p->ZiShu = z;
+    p->next = NULL;
+    if( (Ptrl)->next == NULL )
+        (Ptrl)->next = p;
+    else{
+        q->next = p;
+    }
+    q = p;
+
+}
+List *Sum( List *Ptrl,List *Strl ){
+
+    //什么时候去申请空间？
+    List *NewStack,*tmp,*del;
+    List *P1,*S1;
+    int sum;
+    P1 = Ptrl;
+    S1 = Strl;
+    NewStack = (List*)malloc( sizeof(List) );
+    NewStack->next =  NULL;
+    tmp = NewStack;
+    while( P1 && S1 ){
+
+        if( P1->ZiShu > S1->ZiShu ){
+            tmp->next = P1;
+            P1 = P1->next;
+        }
+        else if( P1->ZiShu < S1->ZiShu ){
+            tmp->next = S1;
+            S1 = S1->next;
+        }
+        //当两个多项式的非零项指数相等时...
+        //直接改变那个系数就行了... 我还想直接申请一个新的临时结点然后指过去...典型的又难又费事
+        //原来一开始方向就偏了,怪不得...
+        else if( P1->ZiShu == S1->ZiShu ){
+            sum = P1->XiShu + S1->XiShu;
+            if( sum ){
+                P1->XiShu = sum;
+                tmp->next = P1;
+                //相等时两个指针都要向后移动
+                P1 = P1->next;
+                S1 = S1->next;
+            }
+        }
+        tmp = tmp->next;
+    }
+    tmp->next = P1 ? P1 : S1;
+    del = NewStack;
+    NewStack = NewStack->next;
+    free(del);
+    return NewStack;
+}
+List *Mult()
+{
+    List *NewStack,*tmp;
+
+}
+void Print( List *Ptrl ){
+
+    while( Ptrl ){
+        cout<<Ptrl->XiShu<<" "<<Ptrl->ZiShu<<" ";
+        Ptrl = Ptrl->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+  List *st1,*st2,*st;
+  st1 = Create();
+  st2 = Create();
+  st = Sum(st1,st2);
+  Print(st);
+
+}
--- queue_array.cpp
+++ queue_array.cpp
@@ -1,66 +1,66 @@
-﻿#include <iostream>
-#include <queue>
-#define ElemenType int
-#define MAXSIZE 10
-using namespace std;
-typedef struct Queue{
-    ElemenType data[MAXSIZE];
-    int head;
-    int last;
-}que;
-que CreateQueue( int MaxSize ){
+﻿//#include <iostream>
+//#include <queue>
+//#define ElemenType int
+//#define MAXSIZE 10
+//using namespace std;
+//typedef struct Queue{
+//    ElemenType data[MAXSIZE];
+//    int head;
+//    int last;
+//}que;
+//que CreateQueue( int MaxSize ){
 
-    que q1;
-    q1.head = 1;
-    q1.last = 1;
-    for( int i = 1; i <= MaxSize; i++ ){
-        q1.last++;
-    }
-    return q1;
-}
-bool Is_filled( que *Ptrl ){
+//    que q1;
+//    q1.head = 1;
+//    q1.last = 1;
+//    for( int i = 1; i <= MaxSize; i++ ){
+//        q1.last++;
+//    }
+//    return q1;
+//}
+//bool Is_filled( que *Ptrl ){
 
-    if( Ptrl->last > MAXSIZE )
-        return false;
+//    if( Ptrl->last > MAXSIZE )
+//        return false;
 
-    return true;
-}
-void Push( que * Ptrl ){
+//    return true;
+//}
+//void Push( que * Ptrl ){
 
-    int count = 1;
-    for( int i = 1; i <= Ptrl->last; i++,count++ ){
-        Ptrl->data[i] = count;
-    }
-}
-void Pop( que *Ptrl ){
+//    int count = 1;
+//    for( int i = 1; i <= Ptrl->last; i++,count++ ){
+//        Ptrl->data[i] = count;
+//    }
+//}
+//void Pop( que *Ptrl ){
 
-    for( int i = Ptrl->head; i <= Ptrl->last; i++ ){
+//    for( int i = Ptrl->head; i <= Ptrl->last; i++ ){
 
-        cout<<Ptrl->data[i]<<" Pop"<<endl;
-        Ptrl->head++;
-    }
-}
-void Print( que * Ptrl ){
+//        cout<<Ptrl->data[i]<<" Pop"<<endl;
+//        Ptrl->head++;
+//    }
+//}
+//void Print( que * Ptrl ){
 
-    if(Ptrl->head > Ptrl->last-1){
-        cout<<"Is_empty"<<endl;
-        return;
-    }
-    for( int i = Ptrl->head; i <= Ptrl->last; i++ )
-        cout<<Ptrl->data[i]<<" ";
-    cout<<endl;
-}
-void Resever( que *Ptrl )
-{
-    for( int i = Ptrl->last; i >= Ptrl->head; i-- )
-        cout<<Ptrl->data[i]<<" ";
-}
-int main()
-{
-    que q2;
-    q2 = CreateQueue(8);
-    Push(&q2);
-//    Pop(&q2);
-    Print(&q2);
-    Resever(&q2);
-}
+//    if(Ptrl->head > Ptrl->last-1){
+//        cout<<"Is_empty"<<endl;
+//        return;
+//    }
+//    for( int i = Ptrl->head; i <= Ptrl->last; i++ )
+//        cout<<Ptrl->data[i]<<" ";
+//    cout<<endl;
+//}
+//void Resever( que *Ptrl )
+//{
+//    for( int i = Ptrl->last; i >= Ptrl->head; i-- )
+//        cout<<Ptrl->data[i]<<" ";
+//}
+//int main()
+//{
+//    que q2;
+//    q2 = CreateQueue(8);
+//    Push(&q2);
+////    Pop(&q2);
+//    Print(&q2);
+//    Resever(&q2);
+//}
