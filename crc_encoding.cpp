307ebe151a9aa3b01741cf101ac7b9b74b5aee08
AmgLi97 <Live97@AMG.com>
2018-11-01 20:46:40 +0800

    2018.11.1 20:46 it's hard...

---
+++ crc_encoding.cpp
@@ -0,0 +1,26 @@
+﻿//#include <iostream>
+//#include <stdlib.h>
+//#include <math.h>
+//#include <string>
+//#define MAX_SIZE 32
+//using namespace std;
+
+//int main()
+//{
+//    string Fx,Gx;
+//    int count;
+//    cin>>Fx>>Gx;
+//    int Fx_arr[Fx.length()];
+//    int Gx_arr[Gx.length()];
+//    count = Gx.length()-1;
+//    Fx = Fx.append(count,'0');
+//    for( int i = 0; i < Fx.length()+count ; i++ ){
+//        Fx_arr[i] = Fx[i] - '0';
+//    }
+//    for( int i = 0; i < Gx.length(); i++ ){
+//        Gx_arr[i] = Gx[i] - '0';
+//    }
+//    //changer to dec
+//}
+
+
--- sequence_list.cpp
+++ sequence_list.cpp
@@ -1,99 +1,117 @@
-﻿#include <iostream>
-#include <stdlib.h>
-#define MAXSIZE 100
-#define ERROR 1
-#define OK    0
-typedef int Status;
-typedef int ElemenType;
-using namespace std;
+﻿///*
+// *
+// * 这是一个顺序表,里面包含增删查改的功能
+// *
+//*/
 
-typedef struct _node{
-    ElemenType *item;
-    ElemenType length;
-}SqList;
 
-Status InitList( SqList & L ){
-    L.item = new ElemenType[MAXSIZE];
-    if( !L.item )
-        return ERROR;
-    L.length = 0;
-    return OK;
-}
-Status Create( SqList & L,ElemenType n ){
+//#include <iostream>
+//#include <stdlib.h>
+//#define MAXSIZE 100
+//#define ERROR 1
+//#define OK    0
+//typedef int Status;
+//typedef int ElemenType;
+//using namespace std;
 
-    int data;
-    for( int i = 1; i <= n; i++ ){
-        cin>>data;
-        L.item[i] = data;
-        L.length++;
-    }
-    return OK;
-}
-Status Print( SqList & L ){
+//typedef struct _node{
+//    ElemenType *item;
+//    ElemenType length;
+//}SqList;
 
-    if( !L.length ){
-        cout<<"no data item"<<endl;
-        return ERROR;
-    }
-    for( int i = 1; i <= L.length; i++ ){
-        cout<<L.item[i]<<" ";
-    }
-    cout<<endl;
-    return OK;
-}
-Status Length( SqList & L ){
-    if( !L.length )
-        return 0;
-    return L.length;
-}
-Status Find( SqList & L, ElemenType n ){
+//Status InitList( SqList & L ){
+//    L.item = new ElemenType[MAXSIZE];
+//    if( !L.item )
+//        return ERROR;
+//    L.length = 0;
+//    return OK;
+//}
+//Status Create( SqList & L,ElemenType n ){
 
-    for( int i = 1; i <= L.length; i++ ){
+//    int data;
+//    for( int i = 1; i <= n; i++ ){
+//        cin>>data;
+//        L.item[i] = data;
+//        L.length++;
+//    }
+//    return OK;
+//}
+//Status Print( SqList & L ){
 
-        if( L.item[i] == n ){
-            cout<<"it's location :"<<i<<endl;
-            return OK;
-        }
-    }
-    cout<<"No found"<<endl;
-    return ERROR;
-}
-Status Insert( SqList & L,int i, ElemenType e ){
+//    if( !L.length ){
+//        cout<<"no data item"<<endl;
+//        return ERROR;
+//    }
+//    for( int i = 1; i <= L.length; i++ ){
+//        cout<<L.item[i]<<" ";
+//    }
+//    cout<<endl;
+//    return OK;
+//}
+//Status Length( SqList & L ){
+//    if( !L.length )
+//        return 0;
+//    return L.length;
+//}
+//Status Find( SqList & L, ElemenType n ){
 
-    if( ( i<1 )|| ( i>L.length ) )
-        return ERROR;
-    if( L.length == MAXSIZE )
-        return ERROR;
-    for( int j = L.length; j >= i; j-- ){
-        L.item[j+1] = L.item[j];
-    }
-    L.item[i] = e;
-    ++L.length;
-    return OK;
-}
-Status Delete( SqList & L,ElemenType e ){
+//    for( int i = 1; i <= L.length; i++ ){
 
-    for( int i = 1; i < L.length; i++ ){
+//        if( L.item[i] == n ){
+//            cout<<"it's location :"<<i<<endl;
+//            return OK;
+//        }
+//    }
+//    cout<<"No found"<<endl;
+//    return ERROR;
+//}
+//Status Insert( SqList & L,int i, ElemenType e ){
 
-        if( L.item[i] == e ){
-            for( int j = i+1; j <= L.length; j++ )
-                L.item[j-1] = L.item[j];
-        }
-    }
-    --L.length;
-    return OK;
-}
-int main()
-{
-    SqList sq1;
-    InitList(sq1);
-    Create(sq1,5);
-    Print(sq1);
-    cout<<"which length :"<<Length(sq1)<<endl;
-    Find(sq1,5);
-    Insert(sq1,3,6);
-    Print(sq1);
-    cout<<"which length :"<<Length(sq1)<<endl;
-    Delete(sq1,5);
-    Print(sq1);
-}
+//    if( ( i<1 )|| ( i>L.length ) )
+//        return ERROR;
+//    if( L.length == MAXSIZE )
+//        return ERROR;
+//    for( int j = L.length; j >= i; j-- ){
+//        L.item[j+1] = L.item[j];
+//    }
+//    L.item[i] = e;
+//    ++L.length;
+//    return OK;
+//}
+//Status Delete( SqList & L,ElemenType e ){
+
+//    for( int i = 1; i < L.length; i++ ){
+
+//        if( L.item[i] == e ){
+//            for( int j = i+1; j <= L.length; j++ )
+//                L.item[j-1] = L.item[j];
+//        }
+//    }
+//    --L.length;
+//    return OK;
+//}
+//Status Destory( SqList & L){
+
+//    while( L.length ){
+//        delete[]L.item;
+//        --L.length;
+//    }
+//    return OK;
+//}
+//int main()
+//{
+//    SqList sq1;
+//    InitList(sq1);
+//    Create(sq1,5);
+//    Print(sq1);
+//    cout<<"which length :"<<Length(sq1)<<endl;
+//    Find(sq1,5);
+//    Insert(sq1,3,6);
+//    Print(sq1);
+//    cout<<"which length :"<<Length(sq1)<<endl;
+//    Delete(sq1,5);
+//    Print(sq1);
+//    Destory(sq1);
+//    Print(sq1);
+
+//}
